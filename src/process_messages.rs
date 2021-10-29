63597b55837cb0272838862150f359e3dd4f6e67
Kyle Machulis <kyle@nonpolynomial.com>
2021-10-28 18:29:09 -0700

    chore: Update device connected message, wrap server in Arc for reuse

--- src/process_messages.rs
+++ src/process_messages.rs
@@ -9,7 +9,7 @@ pub enum EngineMessage {
   EngineStopped,
   ClientConnected(String),
   ClientDisconnected,
-  DeviceConnected(String, u32),
+  DeviceConnected { name: String, index: u32, address: String, display_name: String },
   DeviceDisconnected(u32),
   ClientRejected(String)
 }
