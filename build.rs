95eb440a79dab898168a41e0bb6305f44c789edb
heldom1proton <heldom1@proton.me>
2023-02-18 10:48:48 +0200

    Fix lints.

--- build.rs
+++ build.rs
@@ -10,7 +10,7 @@ use vergen::{vergen, Config, ShaKind};
 fn main() -> Result<()> {
   let out_dir = env::var("OUT_DIR")?;
   let dest_path = Path::new(&out_dir).join("sentry_api_key.txt");
-  let mut f = BufWriter::new(File::create(&dest_path)?);
+  let mut f = BufWriter::new(File::create(dest_path)?);
   // If we have an API key available, save it to a file so we can build it in. If not, leave it
   // blank.
   if let Ok(api_key) = env::var("SENTRY_API_KEY") {
