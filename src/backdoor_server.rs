9d73864903172581431373b75a0296958452fad1
Kyle Machulis <kyle@nonpolynomial.com>
2024-08-25 22:27:29 -0700

    chore: Move backdoor server to just being an IPC to a real server
    Trying to talk directly to the device manager is silly.

--- src/backdoor_server.rs
+++ src/backdoor_server.rs
@@ -1,85 +1,61 @@
 use buttplug::{
   core::{
-    errors::{ButtplugError, ButtplugMessageError},
-    message::{
+    connector::{transport::ButtplugStreamTransport, ButtplugRemoteServerConnector}, message::
       serializer::{
-        ButtplugMessageSerializer, ButtplugSerializedMessage, ButtplugServerJSONSerializer,
-      },
-      ButtplugMessage, ButtplugMessageSpecVersion, ButtplugServerMessage, Error,
-    },
+        ButtplugSerializedMessage, ButtplugServerJSONSerializer,
+      }
+    
   },
-  server::device::ServerDeviceManager,
+  server::{device::ServerDeviceManager, ButtplugServerBuilder}, util::stream::convert_broadcast_receiver_to_stream,
 };
-use futures::{Stream, StreamExt};
 use std::sync::Arc;
+use tokio::sync::{broadcast, mpsc::{self, Sender}};
+use tokio_stream::Stream;
+
+use crate::ButtplugRemoteServer;
 
 // Allows direct access to the Device Manager of a running ButtplugServer. Bypasses requirements for
 // client handshake, ping, etc...
 pub struct BackdoorServer {
-  // The device manager of the server.
-  device_manager: Arc<ServerDeviceManager>,
-  // Unlike clients, which can vary their serializers (but currently don't), we don't expect outside
-  // access to a BackdoorServer, so we can hardcode to using JSON.
-  serializer: ButtplugServerJSONSerializer,
+  //server: ButtplugRemoteServer,
+  sender: Sender<ButtplugSerializedMessage>,
+  broadcaster: broadcast::Sender<String>
 }
 
 impl BackdoorServer {
   pub fn new(device_manager: Arc<ServerDeviceManager>) -> Self {
-    let serializer = ButtplugServerJSONSerializer::default();
-    serializer.force_message_version(&ButtplugMessageSpecVersion::Version3);
+    let server = ButtplugRemoteServer::new(ButtplugServerBuilder::with_shared_device_manager(device_manager.clone()).name("Intiface Backdoor Server").finish().unwrap());
+    let (s_out, mut r_out) = mpsc::channel(255);
+    let (s_in, r_in) = mpsc::channel(255);
+    let (s_stream, _) = broadcast::channel(255);
+    tokio::spawn(async move {
+      if let Err(e) = server.start(ButtplugRemoteServerConnector::<_, ButtplugServerJSONSerializer>::new(ButtplugStreamTransport::new(s_out, r_in))).await {
+        // We can't do much if the server fails, but we *can* yell into the logs!
+        error!("{:?}", e);
+      }
+    });
+    let sender_clone = s_stream.clone();
+    tokio::spawn(async move {
+      while let Some(ButtplugSerializedMessage::Text(m)) = r_out.recv().await {
+        if sender_clone.receiver_count() == 0 {
+          continue;
+        }
+        if sender_clone.send(m).is_err() {
+          break;
+        }
+      }
+    });
     Self {
-      device_manager,
-      serializer,
+      sender: s_in,
+      broadcaster: s_stream
     }
   }
 
   pub fn event_stream(&self) -> impl Stream<Item = String> + '_ {
-    // Unlike the client API, we can expect anyone using the server to pin this
-    // themselves.
-    self
-      .device_manager
-      .event_stream()
-      .map(|x| self.serialize_msg(&x))
+    convert_broadcast_receiver_to_stream(self.broadcaster.subscribe())
   }
 
-  fn serialize_msg(&self, msg: &ButtplugServerMessage) -> String {
-    let serialized_message = self.serializer.serialize(&[msg.clone()]);
-    if let ButtplugSerializedMessage::Text(text_msg) = serialized_message {
-      text_msg
-    } else {
-      panic!("We've hardcoded to use a JSON serializer so we shouldn't get binary back.");
-    }
-  }
-
-  // This has to act like both a message parser *and* a connector, as we're wrapping the serializer
-  // here. So it will only take strings, and only output strings.
-  pub async fn parse_message(&self, msg: &str) -> String {
-    let messages = match self
-      .serializer
-      .deserialize(&ButtplugSerializedMessage::Text(msg.to_owned()))
-    {
-      Ok(msg) => msg,
-      Err(e) => {
-        return self.serialize_msg(
-          &Error::from(ButtplugError::from(
-            ButtplugMessageError::MessageSerializationError(e),
-          ))
-          .into(),
-        )
-      }
-    };
-    let device_manager = self.device_manager.clone();
-    // ID setting is normally done by the top level server, so we'll have to manage that ourselves here.
-    match device_manager.parse_message(messages[0].clone()).await {
-      Ok(mut outgoing_msg) => {
-        outgoing_msg.set_id(messages[0].id());
-        self.serialize_msg(&outgoing_msg)
-      }
-      Err(e) => {
-        let mut error_msg: ButtplugServerMessage = Error::from(e).into();
-        error_msg.set_id(messages[0].id());
-        self.serialize_msg(&error_msg)
-      }
-    }
+  pub async fn parse_message(&self, msg: &str) {
+    self.sender.send(ButtplugSerializedMessage::Text(msg.to_owned())).await.unwrap();
   }
 }
